25d514359ce05719000a79dffef48da921191769
Parp Annop N Ayuttaya <61160112@go.buu.ac.th>
2021-09-29 20:54:41 +0700

    Write test and Set up github

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('GET /', () => {
-    it('responds with "{"message":"Hello world!"}"', (done) => {
+    it('responds with "Hello world!"', (done) => {
         request(app).get('/').expect('{"message":"Hello world!"}', done)
     })
 })
